a46283e828dfdd146b973ec80d8752b774eb05d8
Anbuhp <144266572+Anbuhp@users.noreply.github.com>
2023-10-05 18:41:36 +0530

    Create Ex7.py

---
+++ Ex7.py
@@ -0,0 +1,9 @@
+def factorial(n):
+	if n==1:
+		return 1
+	else:
+		return n*factorial(n-1 )
+num=int(input("Enter a number : "))
+print("The factorial of a {0} is ".
+format(num),factorial(num))
+	
