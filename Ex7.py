6f4fc236e9e8f5e3dfb4126a47d041f90fa8403e
Anbuhp <144266572+Anbuhp@users.noreply.github.com>
2023-10-05 18:43:33 +0530

    Update Ex7.py

--- Ex7.py
+++ Ex7.py
@@ -1,3 +1,4 @@
+Program:
 def factorial(n):
 	if n==1:
 		return 1
@@ -6,4 +7,8 @@ def factorial(n):
 num=int(input("Enter a number : "))
 print("The factorial of a {0} is ".
 format(num),factorial(num))
-	
+
+output:
+
+Enter a number : 5
+The factorial of a 5 is  120
