092059604ba7693aed455c10f34dfb2d89aabe12
Anbuhp <144266572+Anbuhp@users.noreply.github.com>
2023-10-05 19:03:54 +0530

    Create Ex10.py

---
+++ Ex10.py
@@ -0,0 +1,15 @@
+Program:
+
+import math 
+import sys
+print(math .sqrt(25))
+print(math.pi)
+print(math.degrees(2))
+print(math.factorial(4))
+
+output:
+
+5.0
+3.141592653589793
+114.59155902616465
+24
